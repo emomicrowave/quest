6172980e4fa9ad6d3614e9f454a7cc0482adad2d
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2020-06-15 15:54:13 +0200

    docstring and calver

--- quest/__init__.py
+++ quest/__init__.py
@@ -1,10 +1,10 @@
+"""
+A commandline quest log.
+"""
+
 from . import output
 from .task import Task
 from .taskdb import TaskDB, YamlDB
 from .configuration import load_user_configuration
 
-"""
-A commandline quest log.
-"""
-
-__version__ = "0.1"
+__version__ = "2020.06.0"
