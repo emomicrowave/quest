2ec3a5d94c7969445b30176a61748324d6312cb1
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2020-06-15 15:49:34 +0200

    move main to dunder and add docstring and version

--- quest/__init__.py
+++ quest/__init__.py
@@ -2,3 +2,9 @@ from . import output
 from .task import Task
 from .taskdb import TaskDB, YamlDB
 from .configuration import load_user_configuration
+
+"""
+A commandline quest log.
+"""
+
+__version__ = "0.1"
