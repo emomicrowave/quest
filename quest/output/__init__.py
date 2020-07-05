2184a05133c8d7761fb67fda467fa938a5a4e247
Emil Milanov <emo_mlnv@yahoo.com>
2020-07-05 13:10:53 +0200

    Fixed a small bug where reward wasn't shown after completing a task.

--- quest/output/__init__.py
+++ quest/output/__init__.py
@@ -1,2 +1,2 @@
 from .calendar import calendar
-from .lists import taskdb, task, agenda, summary, kanban, all_xp, config_dump
+from .lists import taskdb, task, agenda, summary, kanban, all_xp, config_dump, reward
